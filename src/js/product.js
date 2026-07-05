7732ff2d11f4ed8a9368a98dc8cf4f356e547f92
melcary <melcary13@gmail.com>
2026-07-05 18:29:17 -0500

    fix duplicate import

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,6 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import { getLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
+
 
 const dataSource = new ProductData("tents");
 
