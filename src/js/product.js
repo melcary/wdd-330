8de328d8f2e2b62026e83e0c4b75ca5396daee04
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-07-05 00:06:45 -0300

    solution-to-broken-cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,21 @@
 import { setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import { getLocalStorage } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  
+  let cart = getLocalStorage("so-cart");
+  if (cart === null) {
+    cart = [];
+  }
+  
+  cart.push(product);
+  setLocalStorage("so-cart", cart)
+
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
