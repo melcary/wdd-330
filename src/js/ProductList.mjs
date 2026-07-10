eb907cb6234ef426143c9678d6b5925208b2ca67
emmanuelbulacio4 <emmanuelbulacio4@gmail.com>
2026-07-10 11:19:39 -0300

    W02 Individual - Step4--- ProductData.mjs change line 12 adding "/public/"
    src/index.html reference to js/main.js file
    
    Created ProductList class

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,13 @@
+export default class ProductList{
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+    async init() {
+        const list = await this.dataSource.getData();
+        //Render the list - future
+        
+    }
+
+}
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+
+const producData = new ProductData("tents");
+const productList = new ProductList(); //Faltan los parametros: category, dataSourse, productList
+// productList.init();
