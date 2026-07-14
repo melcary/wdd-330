184461451c2d217e46d5062d3c0d0339225e85ed
melcary <melcary13@gmail.com>
2026-07-14 11:33:29 -0500

    Empty Card Error

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,6 +2,11 @@ import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+   if (!cartItems || cartItems.length === 0) {
+    document.querySelector(".product-list").innerHTML =
+      "<li class='cart-card divider'>Your cart is empty.</li>";
+    return;
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
